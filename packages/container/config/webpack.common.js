f7284f3cd97d4bd95a7641ca95d6e94144c214df
TranHoa1 <tranhoahb99@gmail.com>
2023-05-30 16:59:25 +0700

    Created container deploy script

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,38 +1,27 @@
-// module.exports={
-//     module:{
-//         rules:[
-//             {
-//                 test:/\.m?js$/,
-//                 exclude: /node_modules/,
-//                 use:{
-//                     loader:'babel-loader',
-//                     options:{
-//                         // process all code in react
-//                         // transform code to variety of different ways
-//                         presets:['@babel/preset-react','@babel/preset-env'],
-//                         // add conditional code to run in browser
-//                         plugins:['@babel/plugin-transform-runtime']
-//                     }
-//                 }
-//             }
-//         ]
-//     }
-// }
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-react', '@babel/preset-env'],
-              plugins: ['@babel/plugin-transform-runtime'],
-            },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            // process all code in react
+            // transform code to variety of different ways
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            // add conditional code to run in browser
+            plugins: ["@babel/plugin-transform-runtime"],
           },
         },
-      ],
-    },
-  };
-  
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template:'./public/index.html',
+  })
+  ]
+};
